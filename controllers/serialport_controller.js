81d8c2a2e9f25b5d6ea2e163ff53e80a4199fb51
nattatorn <nattatorn@gmail.com>
2019-03-21 13:06:07 +0700

    clear

--- controllers/serialport_controller.js
+++ controllers/serialport_controller.js
@@ -5,6 +5,7 @@ const parser = new Readline({ delimiter: '\r\n' })
 var THAI_ID_IMGB64 = "";
 var THAI_ID_TEXTB64 = "";
 var DATA_FROM_DEVICE = "";
+var clear = require('clear');
 
 
 const port = new SerialPort(
@@ -25,13 +26,14 @@ port.on('open', () => {
 })
 
 port.on('readable', function () {
+
   DATA_FROM_DEVICE = port.read().toString()
   var rawBit = DATA_FROM_DEVICE.split(" ")
   THAI_ID_IMGB64 = THAI_ID_IMGB64.concat(rawBit[0]);
   if (rawBit[1] != undefined) {
     THAI_ID_TEXTB64 = THAI_ID_TEXTB64.concat(rawBit[1]);
   }
-  console.log(THAI_ID_IMGB64);
+  // console.log(THAI_ID_IMGB64);
 })
 
 exports.writeData = (req, res) => {
@@ -48,9 +50,9 @@ exports.getData = (req, res) => {
   THAI_ID_IMGB64 = THAI_ID_IMGB64.replace(/\n/g, "");
   THAI_ID_TEXTB64 = THAI_ID_TEXTB64.replace(/\n/g, "");
   res.status(200).json({ img64: THAI_ID_IMGB64, text64: THAI_ID_TEXTB64 })
-   THAI_ID_IMGB64 = "";
-   THAI_ID_TEXTB64 = "";
-   DATA_FROM_DEVICE = "";
+var qr = "00020101021153037645802TH29370016A000000677010111011300668992352205406112.00630404E7"
+
 }
+clear();
 
 
