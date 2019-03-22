b1584ebbfa310a87f422b1a2c5a6b8563c85655b
nattatorn <nattatorn@gmail.com>
2019-03-22 13:28:27 +0700

    AllFunction

--- controllers/serialport_controller.js
+++ controllers/serialport_controller.js
@@ -5,7 +5,7 @@ const parser = new Readline({ delimiter: '\r\n' })
 var THAI_ID_IMGB64 = "";
 var THAI_ID_TEXTB64 = "";
 var DATA_FROM_DEVICE = "";
-var clear = require('clear');
+// var clear = require('clear');
 
 
 const port = new SerialPort(
@@ -29,11 +29,14 @@ port.on('readable', function () {
 
   DATA_FROM_DEVICE = port.read().toString()
   var rawBit = DATA_FROM_DEVICE.split(" ")
+
   THAI_ID_IMGB64 = THAI_ID_IMGB64.concat(rawBit[0]);
+  console.log(THAI_ID_IMGB64)
+
   if (rawBit[1] != undefined) {
     THAI_ID_TEXTB64 = THAI_ID_TEXTB64.concat(rawBit[1]);
+    console.log(THAI_ID_TEXTB64)
   }
-  // console.log(THAI_ID_IMGB64);
 })
 
 exports.writeData = (req, res) => {
@@ -50,9 +53,14 @@ exports.getData = (req, res) => {
   THAI_ID_IMGB64 = THAI_ID_IMGB64.replace(/\n/g, "");
   THAI_ID_TEXTB64 = THAI_ID_TEXTB64.replace(/\n/g, "");
   res.status(200).json({ img64: THAI_ID_IMGB64, text64: THAI_ID_TEXTB64 })
-var qr = "00020101021153037645802TH29370016A000000677010111011300668992352205406112.00630404E7"
+
+  THAI_ID_IMGB64 = "";
+  THAI_ID_TEXTB64 = "";
+  DATA_FROM_DEVICE = "";
+  rawBit = ""
+  console.log('clear')
 
 }
-clear();
+// clear();
 
 
